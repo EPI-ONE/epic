ee89ba2eb89fe52a6b21632a1c6ee8a984ca6d94
BgmLover <3150105495@zju.edu.cn>
2019-03-13 21:35:41 +0800

    Add logger and loading config file
    We decide to use spdlog as our logger and TOML as our config file type,
    you can see the doc on the github

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,40 @@
 #include <iostream>
-#include "sstream"
+#include <sstream>
 #include <string>
+
+#include "spdlog/spdlog.h"
+#include "spdlog/sinks/basic_file_sink.h"
 #include "net/peer_manager.h"
 #include "net/net_address.h"
-using namespace std;
-int main(){
-    std::string ip_s = "192.168.0.1:7877";
-    NetAddress ip(ip_s);
-    cout<<ip.ToString();
+#include "utils/cpptoml.h"
+void Use_FileLogger(const std::string& path,const std::string& filename){
+    try
+    {
+        auto file_logger = spdlog::basic_logger_mt("basic_logger", path+filename);
+        spdlog::set_default_logger(file_logger);
+    }
+    catch (const spdlog::spdlog_ex &ex)
+    {
+        std::cout << "Log init failed: " << ex.what() << std::endl;
+    }
 }
 
+void Load_Config_File(){
+    auto config = cpptoml::parse_file("../config.toml");
+    auto log_config = config->get_table("logs");
+    auto use_file_logger = log_config->get_as<bool>("use_file_logger");
+    if(use_file_logger){
+        auto path = log_config->get_as<std::string>("path");
+        auto filename = log_config->get_as<std::string>("filename");
+        Use_FileLogger(*path,*filename);
+    }
+}
+
+
+int main(){
+    Load_Config_File();
+    spdlog::info("Welcome to epic, enjoy your time!");
+    std::string ip_s = "192.168.0.1:7877";
+    NetAddress ip(ip_s);
+    spdlog::info("For test: the ip address is {}",ip.ToString());
+}
