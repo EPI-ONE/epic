a90d031d21aeddacd9259904e7595be8ba281de6
0ersted <zhsang@link.cuhk.edu.hk>
2019-04-12 15:47:14 +0800

    add .clang-format
    block.h is formatted for an example

--- src/block.h
+++ src/block.h
@@ -3,67 +3,72 @@
 
 #include <cstdint>
 
+#include "serialize.h"
 #include "transaction.h"
 #include "uint256.h"
-#include "serialize.h"
 
 class Transaction;
 
 class Block {
-    public:
-        // parameter restrictions
+   public:
+    // parameter restrictions
 
-        // constructor and destructor
-        Block()=default;
-        Block(const Block&) = default;
+    // constructor and destructor
+    Block()             = default;
+    Block(const Block&) = default;
 
-        // Initialize with all the header fields
-        Block(uint32_t version, uint256 milestoneHash, uint256 prevBlockHash,
-              uint256 tipBlockHash, uint256 contentHash, uint32_t time,
-              uint32_t difficultyTarget, uint32_t nonce)
-            : nVersion_(version), hashMilestoneBlock_(milestoneHash),
-              hashPrevBlock_(prevBlockHash), hashTipBlock_(tipBlockHash),
-              hashTransaction_(contentHash), nTime_(time),
-              nBits_(difficultyTarget), nNonce_(nonce) {}
-        ~Block(){};
+    // Initialize with all the header fields
+    Block(uint32_t version,
+        uint256 milestoneHash,
+        uint256 prevBlockHash,
+        uint256 tipBlockHash,
+        uint256 contentHash,
+        uint32_t time,
+        uint32_t difficultyTarget,
+        uint32_t nonce)
+        : nVersion_(version), hashMilestoneBlock_(milestoneHash),
+          hashPrevBlock_(prevBlockHash), hashTipBlock_(tipBlockHash),
+          hashTransaction_(contentHash), nTime_(time), nBits_(difficultyTarget),
+          nNonce_(nonce) {}
+    ~Block(){};
 
-        // daily utils
-        // solve the block maht puzzle
-        void Solve();
-        // verify block content syntactically
-        bool Verify();
-        void AddTransaction(const Transaction& t);
+    // daily utils
+    // solve the block maht puzzle
+    void Solve();
+    // verify block content syntactically
+    bool Verify();
+    void AddTransaction(const Transaction& t);
 
-        ADD_SERIALIZE_METHODS;
+    ADD_SERIALIZE_METHODS;
 
-        template <typename Stream, typename Operation>
-        inline void SerializationOp(Stream& s, Operation ser_action) {
-            READWRITE(nVersion_);
-            READWRITE(hashMilestoneBlock_);
-            READWRITE(hashPrevBlock_);
-            READWRITE(hashTipBlock_);
-            // READWRITE(?); content hash
-            READWRITE(nTime_);
-            READWRITE(nBits_);
-            READWRITE(nNonce_);
-            READWRITE(tx_);
-        }
+    template <typename Stream, typename Operation>
+    inline void SerializationOp(Stream& s, Operation ser_action) {
+        READWRITE(nVersion_);
+        READWRITE(hashMilestoneBlock_);
+        READWRITE(hashPrevBlock_);
+        READWRITE(hashTipBlock_);
+        // READWRITE(?); content hash
+        READWRITE(nTime_);
+        READWRITE(nBits_);
+        READWRITE(nNonce_);
+        READWRITE(tx_);
+    }
 
-        // get & set methods
+    // get & set methods
 
-    private:
-        // header
-        int32_t nVersion_;
-        uint256 hashMilestoneBlock_;
-        uint256 hashPrevBlock_;
-        uint256 hashTipBlock_;
-        uint256 hashTransaction_;
-        uint32_t nTime_;
-        uint32_t nBits_; // difficultyTarget
-        uint32_t nNonce_;
+   private:
+    // header
+    int32_t nVersion_;
+    uint256 hashMilestoneBlock_;
+    uint256 hashPrevBlock_;
+    uint256 hashTipBlock_;
+    uint256 hashTransaction_;
+    uint32_t nTime_;
+    uint32_t nBits_; // difficultyTarget
+    uint32_t nNonce_;
 
-        // content
-        std::vector<Transaction> tx_;
+    // content
+    std::vector<Transaction> tx_;
 };
 
 struct BlockIndex {
