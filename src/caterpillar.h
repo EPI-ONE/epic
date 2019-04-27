ab599b3a61380ec3a4d5e237a3cdc95d8cbb5006
mxar <max.rosssner@gmail.com>
2019-04-27 08:18:47 +0800

    Clean up Caterpiller header

--- src/caterpillar.h
+++ src/caterpillar.h
@@ -1,17 +1,19 @@
 #ifndef __SRC_CATERPILLAR_H__
 #define __SRC_CATERPILLAR_H__
 
-#include "block.h"
-#include "uint256.h"
 #include <list>
 #include <vector>
+
+#include "block.h"
 
 class Caterpillar {
 public:
     // will return a pointer-like structure later
     Block* GetBlock(const uint256 bhash) const;
+
     // flush blocks to db + file storage system
     void Store(const std::vector<Block*> vblock);
+
     // Submits tasks to a single thread in which it topological sorts the blocks
     // and then check their syntax. If all block pass the checking, add them
     // to the corresponding chain in dag_manager.
