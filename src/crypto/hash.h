6ed6b22fd07d2d0da5ab200ead760c0204bdf219
mxar <max.rosssner@gmail.com>
2019-04-28 19:52:10 +0800

    Reformat hash header file

--- src/crypto/hash.h
+++ src/crypto/hash.h
@@ -1,28 +1,29 @@
 #ifndef __SRC_CRYPTO_HASH__
 #define __SRC_CRYPTO_HASH__
 
+#include <vector>
+
 #include "sha256.h"
 #include "stream.h"
 #include "uint256.h"
 
-#include <vector>
-
 /* Compute the 256-bit hash of an object. */
-template<std::size_t R, typename T1>
+template <std::size_t R, typename T1>
 inline uint256 Hash(const T1 pbegin, const T1 pend) {
+    static const unsigned char emptyByte[0] = {};
     uint256 hash;
     CSHA256 sha;
 
-    static const unsigned char emptyByte[0] = {};
-    sha.Write((pbegin == pend) ? emptyByte : (const unsigned char*) &pbegin[0],
-        (pend - pbegin) * sizeof(pbegin[0]));
+    sha.Write((pbegin == pend) ? emptyByte : (const unsigned char*) &pbegin[0], (pend - pbegin) * sizeof(pbegin[0]));
     sha.Finalize((unsigned char*) &hash);
+
 #pragma clang loop unroll_count(R)
     for (size_t i = 1; i < R; i++) {
         sha.Reset();
         sha.Write((unsigned char*) &hash, 32);
         sha.Finalize((unsigned char*) &hash);
     }
+
     return hash;
 }
 
@@ -44,8 +45,8 @@ inline uint160 Hash160(const VStream& vch) {
 }
 
 namespace Hash {
-    const uint256& GetZeroHash();
-    const uint256& GetDoubleZeroHash();
-}
+const uint256& GetZeroHash();
+const uint256& GetDoubleZeroHash();
+} // namespace Hash
 
 #endif
