0566ca4b49a010dab76dc26e36923dd00ee4a34e
mxar <max.rosssner@gmail.com>
2019-04-29 19:10:00 +0800

    Change time_t to uint64_t

--- src/block.h
+++ src/block.h
@@ -88,7 +88,7 @@ protected:
     uint256 milestoneBlockHash_;
     uint256 prevBlockHash_;
     uint256 tipBlockHash_;
-    time_t time_;
+    uint64_t time_;
     uint32_t diffTarget_;
     uint32_t nonce_;
 };
@@ -117,9 +117,9 @@ public:
 
     void SetDifficultyTarget(uint32_t target);
 
-    void SetTime(time_t time);
+    void SetTime(uint64_t time);
 
-    const time_t GetTime() const;
+    const uint64_t GetTime() const;
 
     void SetNonce(uint32_t nonce);
 
