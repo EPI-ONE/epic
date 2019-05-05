ea8596221f4c307dcb68da00b1675a997c8a3d56
mxar <max.rosssner@gmail.com>
2019-05-05 08:22:55 +0800

    Swap private and public block in connection manager definition

--- src/net/connection_manager.h
+++ src/net/connection_manager.h
@@ -22,64 +22,6 @@ typedef std::function<void(void* connection_handle)> delete_connection_callback_
 typedef std::unordered_map<struct bufferevent*, size_t>::iterator bufferevent_map_iter_t;
 
 class ConnectionManager {
-private:
-    struct event_base* base                                 = nullptr;
-    struct evconnlistener* listener                         = nullptr;
-    new_connection_callback_t new_connection_callback       = nullptr;
-    delete_connection_callback_t delete_connection_callback = nullptr;
-
-    std::atomic<bool> interrupt_send_message_;
-
-    std::thread thread_event_base;
-    std::thread thread_send_message;
-
-    std::mutex bev_mtx;
-
-    /* the key is bufferevent, the value is the length of next receive message */
-    std::unordered_map<struct bufferevent*, size_t> bufferevent_map;
-
-    BlockingQueue<NetMessage> receive_message_queue;
-    BlockingQueue<NetMessage> send_message_queue;
-
-    void FreeAllBufferevent_();
-    bool isExist_(struct bufferevent* bev);
-    void ThreadSendMessage_();
-
-    /**
-     * write message bytes to bufferevent output buffer
-     * @param message
-     */
-    void WriteOneMessage_(NetMessage& message);
-
-    /**
-     * read one message from the input buffer, if success then put the message into receive queue
-     * @param bev bufferevent
-     * @param next_message_length
-     * @return true if successful
-     */
-    bool ReadOneMessage_(struct bufferevent* bev, size_t& next_message_length);
-
-    /**
-     * seek next message length in a buffer
-     * @param buf evbuffer
-     * @return the message length to be received
-     */
-    size_t SeekNextMessageLength_(struct evbuffer* buf);
-
-    /**
-     * seek magic number in a buffer
-     * @param buf evbuffer
-     * @return true if found and release the buffer before the magic number
-     */
-    bool SeekMagicNumber_(struct evbuffer* buf);
-
-    /**
-     * seek the payload length in a buffer which start with magic number
-     * @param buf
-     * @return the length of the message payload
-     */
-    size_t SeekMessagePayloadLength_(struct evbuffer* buf);
-
 public:
     ConnectionManager();
     virtual ~ConnectionManager();
@@ -167,6 +109,64 @@ public:
      * @param bev
      */
     void FreeBufferevent(struct bufferevent* bev);
+
+private:
+    struct event_base* base                                 = nullptr;
+    struct evconnlistener* listener                         = nullptr;
+    new_connection_callback_t new_connection_callback       = nullptr;
+    delete_connection_callback_t delete_connection_callback = nullptr;
+
+    std::atomic<bool> interrupt_send_message_;
+
+    std::thread thread_event_base;
+    std::thread thread_send_message;
+
+    std::mutex bev_mtx;
+
+    /* the key is bufferevent, the value is the length of next receive message */
+    std::unordered_map<struct bufferevent*, size_t> bufferevent_map;
+
+    BlockingQueue<NetMessage> receive_message_queue;
+    BlockingQueue<NetMessage> send_message_queue;
+
+    void FreeAllBufferevent_();
+    bool isExist_(struct bufferevent* bev);
+    void ThreadSendMessage_();
+
+    /**
+     * write message bytes to bufferevent output buffer
+     * @param message
+     */
+    void WriteOneMessage_(NetMessage& message);
+
+    /**
+     * read one message from the input buffer, if success then put the message into receive queue
+     * @param bev bufferevent
+     * @param next_message_length
+     * @return true if successful
+     */
+    bool ReadOneMessage_(struct bufferevent* bev, size_t& next_message_length);
+
+    /**
+     * seek next message length in a buffer
+     * @param buf evbuffer
+     * @return the message length to be received
+     */
+    size_t SeekNextMessageLength_(struct evbuffer* buf);
+
+    /**
+     * seek magic number in a buffer
+     * @param buf evbuffer
+     * @return true if found and release the buffer before the magic number
+     */
+    bool SeekMagicNumber_(struct evbuffer* buf);
+
+    /**
+     * seek the payload length in a buffer which start with magic number
+     * @param buf
+     * @return the length of the message payload
+     */
+    size_t SeekMessagePayloadLength_(struct evbuffer* buf);
 };
 
 #endif // EPIC_CONNECTION_MANAGER_H
