e981494fbfcef82b502d4556d37cf5c3754a4c63
mxar <max.rosssner@gmail.com>
2019-05-05 15:46:55 +0800

    Format to_string method in tasm class

--- src/tasm/tasm.cpp
+++ src/tasm/tasm.cpp
@@ -2,10 +2,12 @@
 
 std::string std::to_string(const Tasm::Listing& listing) {
     std::string s = "[";
+
     for (const uint8_t& op : listing.program) {
         s += op;
         s += " ";
     }
+
     s += "]";
     s += "( " + HexStr(listing.data) + " )";
     return s;
