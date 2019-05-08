9eba5e76d4641f989e8ced5ed6c1925585d8a64b
0ersted <zhsang@link.cuhk.edu.hk>
2019-05-08 14:44:19 +0800

    clean code for PR of 119

--- src/caterpillar.h
+++ src/caterpillar.h
@@ -5,78 +5,22 @@
 #include <vector>
 
 #include "block.h"
-#include "dag_manager.h"
-#include "rocksdb.h"
-#include "threadpool.h"
-
-static DAGManager& DAG = DAGManager::GetDAGManager();
-
-class OrphanBlocksContainer {
-public:
-    OrphanBlocksContainer() : thread_(1) {}
-
-    bool Add(const BlockPtr& b);
-
-    void Remove(const BlockPtr& b);
-
-    bool Contains(const uint256& blockHash) const;
-
-    inline bool IsEmpty() {
-        return ODict_.empty();
-    }
-
-    size_t size() {
-        size_t size = 0;
-        for (auto& it: solidDegree_) {
-            size += it.second.size();
-        }
-        return size;
-    }
-
-    /*
-     * Submits a task to thread_ that releases any block from OBC
-     * that becomes solid with blockHash being its antecedent.
-     */
-    void ReleaseBlocks(const uint256& blockHash);
-
-    ~OrphanBlocksContainer();
-
-private:
-    ThreadPool thread_;
-    std::unordered_map<uint256, std::unordered_set<BlockPtr>> ODict_;
-    std::unordered_map<uint8_t, std::unordered_set<uint256>> solidDegree_;
-    std::vector<BlockPtr> updateOne(uint256& blockHash);
-};
 
 class Caterpillar {
 public:
-    Caterpillar() = delete;
-    Caterpillar(std::string& dbPath);
-
     // will return a pointer-like structure later
-    Block* GetBlock(const uint256& bhash) const;
+    Block* GetBlock(const uint256 bhash) const;
 
     // flush blocks to db + file storage system
-    void Store(const std::vector<BlockPtr> vblock);
+    void Store(const std::vector<Block*> vblock);
 
-    /* Submits tasks to a single thread in which it checks its syntax.
-     * If the block passes the checking, add them to the corresponding chain in dag_manager.
-     * Returns true only if the new block successfully updates pending.
-     */
-    bool AddNewBlock(const BlockPtr& block, const Peer& peer);
-
-    ~Caterpillar();
+    // Submits tasks to a single thread in which it topological sorts the blocks
+    // and then check their syntax. If all block pass the checking, add them
+    // to the corresponding chain in dag_manager.
+    void AddNewBlocks(const std::list<const Block> graph);
 
 private:
-    ThreadPool thread_;
-    RocksDBStore dbStore_;
-    OrphanBlocksContainer obc_;
-
-    inline bool Exists(const uint256& blockHash) const {
-        return dbStore_.Exists(blockHash) || obc_.Contains(blockHash);
-    }
-
-    bool CheckPuntuality(const BlockPtr& blk, std::unique_ptr<Block> ms = nullptr) const;
+    std::list<Block&> TopologicalSort(const std::list<const Block&> graph);
 };
 
 #endif // __SRC_CATERPILLAR_H__
