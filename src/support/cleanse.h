82a8f12b474f9ef81d8a0ae57546deb6d0e26f31
mxar <max.rosssner@gmail.com>
2019-05-08 13:40:50 +0800

    Add non-temporal memset to reduce cache pollution
    Further, I moved the function implementations to their own cpp file.

--- src/support/cleanse.h
+++ src/support/cleanse.h
@@ -2,17 +2,25 @@
 #define EPIC_CLEANSE_H
 
 #define __STDC_WANT_LIB_EXT1__ 1
+
+#include <cassert>
 #include <cstring>
+#include <immintrin.h>
+
+void* memset_tmp(void* ptr, int value, std::size_t len);
+void* memset_ntmp(void* ptr, int value, std::size_t len);
 
 typedef void* (*memset_t)(void*, int, std::size_t);
-static volatile memset_t memset_safe = std::memset;
+static volatile memset_t memset_safe_tmp  = memset_tmp;
+static volatile memset_t memset_safe_ntmp = memset_ntmp;
 
+template <bool FinalCleanse = false>
 inline void memory_cleanse(void* ptr, std::size_t len) {
-#ifdef __STDC_LIB_EXT1__
-    memset_s(ptr, len, 0, len);
-#else
-    memset_safe(ptr, 0, len);
-#endif
+    if (FinalCleanse) {
+        memset_safe_ntmp(ptr, 0, len);
+    } else {
+        memset_safe_tmp(ptr, 0, len);
+    }
 }
 
 #endif
