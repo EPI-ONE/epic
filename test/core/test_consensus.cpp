82588dbdafde38445b9bcb2d14c2ca3d7b29f97f
Val <valeryxbaturin@gmail.com>
2019-05-08 16:17:22 +0800

    Delete BlockHeader and introduce BlockNet and BlockDag
    Merge BlockHeader and Block to the Block class.
    Create BlockDAG and BlockNet children of the Block class (`block.*` files).
    Overload serialization of Block in children classes such as BlockDAG ~ (De)SerializeTo(From)DB and BlockNet ~ (De)Serialize of the current Block.
    Overload GetOptimalEncodingSize accordingly and move optimalEncodingSize from private to protected. Modify tests to use new block abstractions.

--- test/core/test_consensus.cpp
+++ test/core/test_consensus.cpp
@@ -6,7 +6,7 @@ typedef Tasm::Listing Listing;
 
 class ConsensusTest : public testing::Test {};
 
-Block FakeBlock(int numTxInput = 0, int numTxOutput = 0, bool solve = false) {
+BlockNet FakeBlock(int numTxInput = 0, int numTxOutput = 0, bool solve = false) {
     uint256 r1;
     r1.randomize();
     uint256 r2;
@@ -14,7 +14,7 @@ Block FakeBlock(int numTxInput = 0, int numTxOutput = 0, bool solve = false) {
     uint256 r3;
     r3.randomize();
 
-    Block b = Block(BlockHeader(1, r1, r2, r3, time(nullptr), 0x1f00ffffL, 0));
+    BlockNet b = Block(1, r1, r2, r3, time(nullptr), 0x1f00ffffL, 0);
 
     if (numTxInput || numTxOutput) {
         Transaction tx;
@@ -40,22 +40,42 @@ Block FakeBlock(int numTxInput = 0, int numTxOutput = 0, bool solve = false) {
 }
 
 TEST_F(ConsensusTest, SyntaxChecking) {
-    Block b = GENESIS;
+    BlockNet b = GENESIS;
     EXPECT_TRUE(b.Verify());
 
     // Create a random block with bad difficulty target
     uint256 rand256;
     rand256.randomize();
     uint256 zeros;
-    Block block = Block(BlockHeader(1, rand256, zeros, rand256, time(nullptr), 1, 1));
+    BlockNet block = Block(1, rand256, zeros, rand256, time(nullptr), 1, 1);
     EXPECT_FALSE(block.Verify());
 }
 
-TEST_F(ConsensusTest, OptimalEncodingSize) {
-    Block b = GENESIS;
+
+TEST_F(ConsensusTest, BlockDagOptimalEncodingSize) {
+    BlockDag b = GENESIS;
     EXPECT_EQ(VStream(b).size(), b.GetOptimalEncodingSize());
 
-    Block b1 = FakeBlock();
+    BlockDag b1 = FakeBlock();
+
+    // test without a tx
+    EXPECT_EQ(VStream(b1).size(), b1.GetOptimalEncodingSize());
+
+    // with a big-enough tx to test the variable-size ints (e.g., VarInt, CompactSize)
+    Transaction tx;
+    for (int i = 0; i < 512; ++i) {
+        tx.AddInput(TxInput(Hash::GetZeroHash(), i, Tasm::Listing(VStream(i))));
+        tx.AddOutput(TxOutput(i, Tasm::Listing(VStream(i))));
+    }
+    b1.AddTransaction(tx);
+    EXPECT_EQ(VStream(b1).size(), b1.GetOptimalEncodingSize());
+}
+
+TEST_F(ConsensusTest, BlockNetOptimalEncodingSize) {
+    BlockNet b = GENESIS;
+    EXPECT_EQ(VStream(b).size(), b.GetOptimalEncodingSize());
+
+    BlockNet b1 = FakeBlock();
 
     // test without a tx
     EXPECT_EQ(VStream(b1).size(), b1.GetOptimalEncodingSize());
@@ -71,9 +91,9 @@ TEST_F(ConsensusTest, OptimalEncodingSize) {
 }
 
 TEST_F(ConsensusTest, UTXO) {
-    Block b             = FakeBlock(1, 67, true);
-    UTXO utxo           = UTXO(b.GetTransaction()->GetOutput(66), 66);
-    uint256 key         = utxo.GetKey();
+    BlockNet b  = FakeBlock(1, 67, true);
+    UTXO utxo   = UTXO(b.GetTransaction()->GetOutput(66), 66);
+    uint256 key = utxo.GetKey();
 
     arith_uint256 bHash = UintToArith256(b.GetHash());
     arith_uint256 index = arith_uint256("0x4200000000000000000000000000000000000000000000000000000000");
