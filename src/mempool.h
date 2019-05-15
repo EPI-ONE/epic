d0328160a7aa62a4259b88decb78722ac092c629
mxar <max.rosssner@gmail.com>
2019-05-15 10:10:52 +0800

    Add comments to MemPool definition

--- src/mempool.h
+++ src/mempool.h
@@ -12,15 +12,26 @@ class MemPool {
 public:
     MemPool() = default;
 
+    /* insert a transaction into the mempool iff there
+     * is no transaction in the pool with the same hash;
+     * the return value indicates wether the value was
+     * inserted */
     bool Insert(ConstTxPtr value);
 
     bool Contains(ConstTxPtr value) const;
 
+    /* retrives the first transaction from the pool that has
+     * a sortition distance smaller or equal to the distance given */
     std::optional<ConstTxPtr> GetTransaction(const uint256& BlockHash, const arith_uint256& distance);
 
+    /* removes all transaction for which
+     * std::equal_to<ConstTxPtr> is true
+     * for the given value */
     bool Erase(ConstTxPtr value);
 
 private:
+    /* the set is locked into memory using the locked allocator;
+     * this has the beneficial effect that it can never be swapped out */
     std::unordered_set<ConstTxPtr, std::hash<ConstTxPtr>, std::equal_to<ConstTxPtr>, locked_allocator<ConstTxPtr>>
         mempool_;
 };
