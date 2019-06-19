d680e96004cb677b8c3c92ee0cfb6b6349322753
bassland <robotsky8@gmail.com>
2019-06-19 20:46:13 +0800

    fix peermanager testcase

--- test/net/test_peer_manager.cpp
+++ test/net/test_peer_manager.cpp
@@ -2,52 +2,46 @@
 #include "peer_manager.h"
 #include <gtest/gtest.h>
 
-//class TestPeerManager : public testing::Test {
-//public:
-    //PeerManager server;
-    //PeerManager client;
+class TestPeerManager : public testing::Test {
+public:
+    PeerManager server;
+    PeerManager client;
 
-    //static void SetUpTestCase() {
-        //char** s = new char*[1];
-        //s[0]     = new char[5];
-        //strcpy(s[0], "epic");
-        //Init(1, s);
+    static void SetUpTestCase() {
+        config = std::make_unique<Config>();
+    }
+    static void TearDownTestCase() {
+        ShutDown();
+    }
+    void SetUp() {
+        server.Start();
+        client.Start();
+    }
 
-        //delete[] s[0];
-        //delete[] s;
-    //}
-    //static void TearDownTestCase() {
-        //ShutDown();
-    //}
-    //void SetUp() {
-        //server.Start();
-        //client.Start();
-    //}
+    void TearDown() {
+        server.Stop();
+        client.Stop();
+    }
+};
 
-    //void TearDown() {
-        //server.Stop();
-        //client.Stop();
-    //}
-//};
+TEST_F(TestPeerManager, CallBack) {
+    EXPECT_TRUE(server.Bind("127.0.0.1:7877"));
+    EXPECT_TRUE(client.ConnectTo("127.0.0.1:7877"));
+    usleep(50000);
+    EXPECT_EQ(server.GetFullyConnectedPeerSize(), 1);
+    EXPECT_EQ(client.GetFullyConnectedPeerSize(), 1);
+}
 
-//TEST_F(TestPeerManager, CallBack) {
-    //EXPECT_TRUE(server.Bind("127.0.0.1:7877"));
-    //EXPECT_TRUE(client.ConnectTo("127.0.0.1:7877"));
-    //usleep(50000);
-    //EXPECT_EQ(server.GetFullyConnectedPeerSize(), 1);
-    //EXPECT_EQ(client.GetFullyConnectedPeerSize(), 1);
-//}
+TEST_F(TestPeerManager, CheckHaveConnectedSameIP) {
+    EXPECT_TRUE(server.Bind("127.0.0.1:7877"));
+    EXPECT_TRUE(client.ConnectTo("127.0.0.1:7877"));
+    usleep(50000);
 
-//TEST_F(TestPeerManager, CheckHaveConnectedSameIP) {
-    //EXPECT_TRUE(server.Bind("127.0.0.1:7877"));
-    //EXPECT_TRUE(client.ConnectTo("127.0.0.1:7877"));
-    //usleep(50000);
-
-    //PeerManager same_ip_client;
-    //same_ip_client.Start();
-    //same_ip_client.ConnectTo("127.0.0.1:7877");
-    //usleep(50000);
-    //EXPECT_EQ(server.GetFullyConnectedPeerSize(), 1);
-    //EXPECT_EQ(same_ip_client.GetConnectedPeerSize(), 0);
-    //same_ip_client.Stop();
-//}
+    PeerManager same_ip_client;
+    same_ip_client.Start();
+    same_ip_client.ConnectTo("127.0.0.1:7877");
+    usleep(50000);
+    EXPECT_EQ(server.GetFullyConnectedPeerSize(), 1);
+    EXPECT_EQ(same_ip_client.GetConnectedPeerSize(), 0);
+    same_ip_client.Stop();
+}
