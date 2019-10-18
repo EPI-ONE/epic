6f149a8864702b270a0097bf9079677ad2a05a02
Rei J. Zhang <jiheng@ust.hk>
2019-10-18 15:08:53 +0800

    solve libevent cmake fail

--- ci/install-libevent.sh
+++ ci/install-libevent.sh
@@ -3,11 +3,12 @@ set -ev
 EPIC_LIBEVENT_VERSION="2.1.11"
 EPIC_LIBEVENT_DIR=""libevent-"$EPIC_LIBEVENT_VERSION"
 
+sudo rm -rf libevent*
 if [[ ! -d ${EPIC_LIBEVENT_DIR} ]];then
     sudo rm -rf libevent*
     git clone -b release-${EPIC_LIBEVENT_VERSION}-stable --single-branch https://github.com/libevent/libevent.git ${EPIC_LIBEVENT_DIR}
     cd ${EPIC_LIBEVENT_DIR} && mkdir build && cd build
-    echo $OPENSSL_ROOT_DIR $OPENSSL_INCLUDE_DIR
+    echo $OPENSSL_ROOT_DIR 
     cmake ..
     make -j6
 else
