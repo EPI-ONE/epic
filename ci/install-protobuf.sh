8c816f2cb42f409a25542de494d6925aa44307f0
Rei <jiheng@ust.hk>
2019-10-24 23:23:15 +0800

    Testnet 0.1.0 (#146)
    Release Testnet 0.1.0
    
    - Add instructions for joining the Testnet.
    - Update install instructions
    - Upgrade `cxxopt` and `spdlog`
    - Streamline ci install scripts
    - Systematically install dependencies by using `./sys-dep-install.sh`
    - Drop support for CentOS
    - upgrade to gcc-8 and clang-9 for both travis and install
    - Remove Travis deploy, plan to use Github Action

--- ci/install-protobuf.sh
+++ ci/install-protobuf.sh
@@ -3,21 +3,17 @@ set -ev
 EPIC_PROTOBUF_VERSION="3.10.0"
 EPIC_PROTOBUF_DIR=""protobuf-"$EPIC_PROTOBUF_VERSION"
 
-if [ ${TRAVIS_OS_NAME} == 'osx' ]; then
-    brew install protobuf 
+if [[ ! -d ${EPIC_PROTOBUF_DIR} ]];then
+    sudo rm -rf protobuf*
+    git clone -b v${EPIC_PROTOBUF_VERSION} --single-branch https://github.com/protocolbuffers/protobuf.git ${EPIC_PROTOBUF_DIR}
+    cd ${EPIC_PROTOBUF_DIR}
+    git submodule update --init --recursive
+    ./autogen.sh && ./configure && make -j6
+    #make check
 else
-    if [[ ! -d ${EPIC_PROTOBUF_DIR} ]];then
-        sudo rm -rf protobuf*
-        git clone -b v${EPIC_PROTOBUF_VERSION} --single-branch https://github.com/protocolbuffers/protobuf.git ${EPIC_PROTOBUF_DIR}
-        cd ${EPIC_PROTOBUF_DIR}
-        git submodule update --init --recursive
-        ./autogen.sh && ./configure && make -j6
-        #make check
-    else
-        cd ${EPIC_PROTOBUF_DIR}
-    fi
-
-    sudo make install
-    sudo ldconfig # refresh shared library cache.
-    cd ..
+    cd ${EPIC_PROTOBUF_DIR}
 fi
+
+sudo make install
+sudo ldconfig # refresh shared library cache.
+cd ..
---
+++ ci/sys-dep-install.sh
@@ -0,0 +1,64 @@
+#!/usr/bin/env bash
+# set -ev
+EPIC_SYS_DEP_DIR="sys-dep-install"
+EPIC_SYS_INFO="$(uname -v)"
+
+echo $EPIC_SYS_INFO
+
+export LANG="en_US.UTF-8"
+export LC_ALL=
+
+if [[ ! -d ${EPIC_SYS_DEP_DIR} ]]
+then
+  mkdir ${EPIC_SYS_DEP_DIR}
+fi
+
+cd ${EPIC_SYS_DEP_DIR} 
+
+if [[ ${EPIC_SYS_INFO} == *"Darwin"* ]] && [[ ${EPIC_SYS_INFO} == *"Version 19."* ]]
+then
+  brew install autoconf automake libtool
+  brew install openssl@1.1
+  export OPENSSL_ROOT_DIR="/usr/local/opt/openssl@1.1"
+  brew install cmake
+  brew install gmp
+  brew install grpc
+  brew install rocksdb
+  brew install protobuf
+
+  ../install-secp256k1.sh &
+  ../install-libevent.sh &
+  ../install-googletest.sh &
+
+elif [[ ${EPIC_SYS_INFO} == *"Ubuntu"* ]] && [[ ${EPIC_SYS_INFO} == *"18.04"* ]]
+then
+  sudo apt-get install autoconf libtool pkg-config
+  sudo apt-get install libgmp-dev
+
+  # install gcc 8
+  sudo apt-get install gcc-8 g++-8
+  # update alternatives
+  sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-8 100
+  sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-8 100
+
+  # install llvm clang
+  wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | sudo apt-key add -
+  sudo apt-add-repository "deb http://apt.llvm.org/bionic/ llvm-toolchain-bionic-9 main"
+  sudo apt-get update 
+  sudo apt-get install clang-9
+  # update alternatives
+  sudo update-alternatives --install /usr/bin/clang clang /usr/bin/clang-9 100
+  sudo update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-9 100
+
+  ../install-cmake.sh 
+  ../install-openssl.sh
+
+  ../install-secp256k1.sh &
+  ../install-libevent.sh &
+  ../install-googletest.sh &
+  ../install-protobuf.sh &
+  ../install-grpc.sh &
+  ../install-rocksdb.sh &
+else
+  echo "This script only supports Mac OS X and Ubuntu Linux 18.04"
+fi
