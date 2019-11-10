8840168811bd66f1b535d81101857d69dc4cdbc0
Rei J. Zhang <jiheng@ust.hk>
2019-11-10 20:30:14 +0800

    downgrade rocksdb

--- ci/install-rocksdb.sh
+++ ci/install-rocksdb.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 set -ev
-EPIC_ROCKSDB_VERSION="6.4.6"
+EPIC_ROCKSDB_VERSION="6.3.6"
 EPIC_ROCKSDB_DIR=""rocksdb-"$EPIC_ROCKSDB_VERSION"
 
 if [[ ! -d ${EPIC_ROCKSDB_DIR} ]];then
     sudo rm -rf rocksdb*
     git clone -b v${EPIC_ROCKSDB_VERSION} --single-branch https://github.com/facebook/rocksdb.git ${EPIC_ROCKSDB_DIR}
     cd ${EPIC_ROCKSDB_DIR}
-    make shared_lib -j6
+    make shared_lib -j4
 else
     cd ${EPIC_ROCKSDB_DIR}
 fi
